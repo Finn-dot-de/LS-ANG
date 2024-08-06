39df8bbfa55c88205a4c146bdf4800ec4a3c906e
LY04 <Finn.Laskowski@bamf.bund.de>
2024-08-06 08:35:39 +0200

    Nicht verwendete Imports entfernt

--- src/app/Components/Subcomponents/login/login.component.ts
+++ src/app/Components/Subcomponents/login/login.component.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
-import { FormsModule, NgModel } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 import { AuthService } from '../../../auth.service';
 
 @Component({
--- src/app/Components/maincomponents/dropdown/dropdown.component.ts
+++ src/app/Components/maincomponents/dropdown/dropdown.component.ts
@@ -1,7 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterModule, Routes } from '@angular/router';
-import { AuthService } from 'src/app/auth.service';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-dropdown',
